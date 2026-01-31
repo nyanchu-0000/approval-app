08bd2f5179cd734a4a5fa7105db29942c490098e
nyanchu-0000 <yuinezu.0110@gmail.com>
2026-01-31 12:21:57 +0900

    警告削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoadingPage } from './pages/LoadingPage';
 import { HomePage } from './pages/HomePage';
